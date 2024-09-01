472869f60540251aac98532b7a4232749057900f
Chanell Cousin <nelltheowl@gmail.com>
2024-08-31 21:58:05 -0400

    hgjhgjg

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("August 30, 2024 00:00:00").getTime();
+    const countDate = new Date("September 6, 2024 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
